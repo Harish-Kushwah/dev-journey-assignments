884d3d16362b3090727c03988879b3bf1c1cf2c1
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-09-12 23:31:08 +0530

    magic square example updated

--- AAC/31-07-24/Ex6.cpp
+++ AAC/31-07-24/Ex6.cpp
@@ -1,78 +1,43 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-
-const int n = 3;
 
 //6.Write an algorithm to create a magic square.
+int main(){
 
-int getUniqueRandomNumber(int arr[],int n){
-    int index = rand()%n;
-    while(arr[index]==0){
-       index = rand()%4;
-    }
-    int num  = arr[index];
-    arr[index] = 0;
-    return num;
-}
-int printSquare(int square[n][n]){
+    int n;
+    printf("Enter n (only odd number):");
+    scanf("%d",&n);
+
+   int arr[n][n];
+
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            printf("%d ",square[i][j]);
+           arr[i][j] = 0;
+        }
+    }
+
+   int row = 0;
+   int col =n/2;
+
+   for(int i=1;i<=n*n;i++){
+        arr[row][col] = i;
+
+        int newRow = (row-1+n)%n;
+        int newCol = (col+1)%n;
+
+        if(arr[newRow][newCol]!=0){
+            row = (row+1)%n;
+        }
+        else{
+            row = newRow;
+            col = newCol;
+        }
+   }
+
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
         }
         printf("\n");
     }
-}
-int isMagicSquare(int square[n][n]){
-    int row1 = square[0][0] + square[0][1] + square[0][2];
-    int row2 = square[1][0] + square[1][1] + square[1][2];
-    int row3 = square[2][0] + square[2][1] + square[2][2];
 
-    int col1 = square[0][0] + square[1][0] + square[2][0];
-    int col2 = square[0][1] + square[1][1] + square[2][1];
-    int col3 = square[0][2] + square[1][2] + square[2][2];
-
-    int dig1 = square[0][0] + square[1][1] + square[2][2];
-    int dig2 = square[2][0] + square[1][1] + square[0][2];
-
-
-    return (row1==row2 && row2==row3 && row3==15) &&  
-           (col1==col2 &&col2==col3 && col3==15) &&
-           (dig1==dig2 && dig2==15);
-}
-void printMagicSquare(){
-    //Corner Number's(Even no.):2,4,6,8 
-    //other except corner and middle : 1,3,7,9
-    //center Number : 5
-    int square[n][n];
-    do
-    {
-        int even[] = {2,4,6,8};
-        int odd[] = {1,3,7,9};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if((i==0 &&(j==0 || j==n-1)) || (i==n-1 &&(j==0 || j==n-1)) ){
-                    //for even numbers
-                    square[i][j] = getUniqueRandomNumber(even,4);
-                }
-                else if(i==n/2 && j==n/2){ 
-                    square[i][j] = 5; 
-                }
-                else{
-                    //for odd numbers
-                    square[i][j] = getUniqueRandomNumber(odd,4);  
-                }
-            }
-        }
-    }while(!isMagicSquare(square));
-
-    printSquare(square);
-}
-int main(){
-    srand(time(NULL)); 
-    for(int i=0;i<10;i++){
-        printf("Magic Square : %d\n",(i+1));
-        printMagicSquare();
-        printf("\n");
-    }
 }
